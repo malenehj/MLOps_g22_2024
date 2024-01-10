eb970542e0d2e1a18c2736b7eefee4e4ed812040
Mathias <matnis1995@gmail.com>
2024-01-10 15:24:11 +0100

    first proper implimentation of a test

--- tests/test_make_dataset.py
+++ tests/test_make_dataset.py
@@ -1,10 +1,18 @@
 import os
 import pytest
 
-os.system('ls')
-
+from torch.utils.data import DataLoader
 from mlops_g22_2024.data import make_dataset
 
 def test_basic_data_import():
-    D = make_dataset.md()
-    print('this is a print')
+    train_dataloader, val_dataloader, test_dataloader = make_dataset.md()
+
+    # Testing if the dataloaders are of type DataLoader:
+    assert type(train_dataloader) == DataLoader
+    assert type(val_dataloader) == DataLoader
+    assert type(test_dataloader) == DataLoader
+
+    # Testing if the dataloaders are not empty:
+    assert len(train_dataloader) > 0
+    assert len(val_dataloader) > 0
+    assert len(test_dataloader) > 0
