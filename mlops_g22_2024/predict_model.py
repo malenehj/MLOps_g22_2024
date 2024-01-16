dcfb8e1e6a1ca541f7e91748da665e68dbc6f43d
malenehj <100592523+malenehj@users.noreply.github.com>
2024-01-16 14:29:19 +0100

    Update path to model in predict_model.py

--- mlops_g22_2024/predict_model.py
+++ mlops_g22_2024/predict_model.py
@@ -6,7 +6,7 @@ def predict(text: str):
     #text = input('Write a sentence: ')  # the user inputs text to be predicted
 
     current_dir = os.path.dirname(__file__)  # gets the directory where the predict_model script is located
-    relative_path = os.path.join(current_dir,'outputs/2024-01-15/models') # navigates to the saved model
+    relative_path = os.path.join(current_dir,'../models') # navigates to the saved model
 
     classifier = pipeline("text-classification", model=relative_path)
 
