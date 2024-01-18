c5649ea131e222f66079798c0b4605f1cd5529ad
ilFuma <rc@medlay.vc>
2024-01-18 16:09:30 +0100

    test train

--- tests/test_train_model.py
+++ tests/test_train_model.py
@@ -2,7 +2,7 @@ import os
 from transformers import DistilBertTokenizerFast
 from mlops_g22_2024.train_model import main
 
-def test_main():
+def test_train_model_saved():
     # Define the configuration for testing
     config = {
         "train": {
@@ -18,31 +18,30 @@ def test_main():
     os.environ["USE_WANDB"] = "false"
 
     # Call the main function
-    main(config)
+    os.chdir("../mlops_g22_2024")
+    # main()
+    train_dict = {
+    "lr": 2e-5,
+    "train_batch_size": 16,
+    "eval_batch_size": 16,
+    "epochs": 2,
+    "weight_decay": 0.01
+}
+    
+    # print()
+    # print()
+    # print()
+    # print("marioclioairoarioa")
+    # print()
+    # print()
+
+
+    config_dict = {"train": train_dict}
+    main(config_dict)
+
 
     # Check if the trained model exists
     model_path = '../models'
     assert os.path.exists(model_path)
 
-    # Load the trained model and tokenizer
-    tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
-    model = AutoModelForSequenceClassification.from_pretrained(model_path)
-
-    # Perform some sample predictions
-    text_happy = "my life is amazing"
-    text_tired = "I am very tired today"
-
-    inputs_happy = tokenizer(text_happy, return_tensors="pt")
-    inputs_tired = tokenizer(text_tired, return_tensors="pt")
-
-    outputs_happy = model(**inputs_happy)
-    outputs_tired = model(**inputs_tired)
-
-    # Check if the model predicts the labels correctly
-    expected_emotions = ['anger', 'fear', 'joy', 'love', 'sadness', 'surprise']
-
-    assert outputs_happy.logits.argmax().item() in expected_emotions
-    assert outputs_tired.logits.argmax().item() in expected_emotions
-
-    # Clean up any temporary files or directories
-    os.remove(model_path)
+    
